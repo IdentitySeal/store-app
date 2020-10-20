ddb2a9350c1d55ce90b561a2ef0edd83a8934eb9
unknown <teemealeheen@gmail.com>
2020-10-20 22:02:57 +0100

    setting up environment for developmant

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md">Hey</div>
+        <div className="col-md">Hey</div>
+        <div className="col-md">Hey</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
